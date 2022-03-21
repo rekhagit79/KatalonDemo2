894abc1c6a9927d195dc306534563a7319a3f058
Rekha Chathoth <rekha.chathoth@gmail.com>
2022-03-21 13:25:11 -0400

    git2 commit

---
+++ Test Suites/Git2.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Git2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>c71bf881-134c-4376-bacc-27dba0716b27</testSuiteGuid>
+</TestSuiteEntity>
